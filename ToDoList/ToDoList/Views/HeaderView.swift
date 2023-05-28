5ac38709898dd23566d798782ffcb6bc432854b3
kagankuscu <kuscukagan@gmail.com>
2023-05-28 10:27:13 +0300

    Create HeaderView.swift

---
+++ ToDoList/ToDoList/Views/HeaderView.swift
@@ -0,0 +1,43 @@
+//
+//  HeaderView.swift
+//  ToDoList
+//
+//  Created by Kagan Kuscu on 26.05.23.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    let title: String
+    let subTitle: String
+    let angle: Double
+    let background: Color
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 0)
+                .foregroundColor(background)
+                .rotationEffect(Angle(degrees: angle))
+            
+            VStack {
+                Text(title)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                Text(subTitle)
+                    .font(.headline)
+                    .foregroundColor(.white)
+            }
+            .padding(.top, 80)
+        }
+        .frame(width: UIScreen.main.bounds.width * 3,
+               height: 350)
+        .offset(y: -150)
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView(title: "Title", subTitle: "subTitle", angle: 15, background: .pink)
+    }
+}
