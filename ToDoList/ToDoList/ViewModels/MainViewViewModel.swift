dd8b25abb73152a6197ccd4f06d6f2e739057655
kagankuscu <kuscukagan@gmail.com>
2023-05-28 10:29:51 +0300

    Update MainViewViewModel.swift

--- ToDoList/ToDoList/ViewModels/MainViewViewModel.swift
+++ ToDoList/ToDoList/ViewModels/MainViewViewModel.swift
@@ -5,4 +5,22 @@
 //  Created by Kagan Kuscu on 24.05.23.
 //
 
+import FirebaseAuth
 import Foundation
+
+class MainViewViewModel: ObservableObject {
+    @Published var currentUserId: String = ""
+    private var handler: AuthStateDidChangeListenerHandle?
+    
+    init() {
+        self.handler = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            DispatchQueue.main.async {
+                self?.currentUserId = user?.uid ?? ""
+            }
+        }
+    }
+    
+    public var isSignedIn: Bool {
+        return Auth.auth().currentUser != nil
+    }
+}
