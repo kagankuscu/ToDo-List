8018ea6f920edd4b20a561b451bef7c1fb3402f8
kagankuscu <kuscukagan@gmail.com>
2023-05-28 10:28:05 +0300

    Register view and viewModel added

--- ToDoList/ToDoList/Views/RegisterView.swift
+++ ToDoList/ToDoList/Views/RegisterView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @StateObject var viewModel = RegisterViewViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView(title: "Register",
+                       subTitle: "Start organizing todos",
+                       angle: -15,
+                       background: .orange)
+            
+            Form {
+                TextField("Full Name", text: $viewModel.name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                
+                TextField("Email Address", text: $viewModel.email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                
+                SecureField("Password", text: $viewModel.password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                
+                TLButton(title: "Create Account",
+                         background: .green
+                ) {
+                    viewModel.register()
+                }
+            }
+            .offset(y: -50)
+            
+            Spacer()
+        }
     }
 }
 
