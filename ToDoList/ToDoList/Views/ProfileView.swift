a9d1427116cb6cd124cc561ee23d68b49f5df7d6
kagankuscu <kuscukagan@gmail.com>
2023-05-30 10:25:31 +0300

    Log out and Profile view added

--- ToDoList/ToDoList/Views/ProfileView.swift
+++ ToDoList/ToDoList/Views/ProfileView.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewViewModel()
+    
     var body: some View {
         NavigationView {
             VStack {
+                if let user = viewModel.user {
+                    profile(user: user)
+                }else {
+                    Text("Loading Profile...")
+                }
                 
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.blue)
+            .frame(width: 125, height: 125)
+        
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                Text(user.name)
+            }
+            .padding()
+            HStack {
+                Text("Email: ")
+                Text(user.email)
+            }
+            .padding()
+            HStack {
+                Text("Member since: ")
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        
+        Button("Log Out") {
+            viewModel.logOut()
+        }
+        .foregroundColor(.red)
+        
+        Spacer()
     }
 }
 
